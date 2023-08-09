24ae551e601d1f45f47574dd11786e7f447b156e
Anshul <anshulgayakwad12345@gmail.com>
2023-08-09 16:04:03 +0530

    2nd Answer

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,6 +15,6 @@ class B extends A{
 public class Main {
     public static void main(String args[]){
         B b = new B();
-        System.out.println(b.meth());
+        //System.out.println(b.meth());
     }
 }
